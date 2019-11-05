f69f9608f08ca76287a82ed1f2ba7c7cc87ea10c
felipefln <felipecarvalho_22_@hotmail.com>
2019-11-04 22:43:28 -0300

    Create routes from project router pages

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Login from './components/Login'
+import CreateCustomers from './components/CreateCustomers'
+import EditCustomers from './components/EditCustomers'
+import ListCustomers from './components/ListCustomers'
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/create" exact component={CreateCustomers}/>
+                <Route path="/edit" exact component={EditCustomers}/>
+                <Route path="/list" exact component={ListCustomers}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
